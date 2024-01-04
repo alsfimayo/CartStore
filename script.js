43d6700dc7aed5386db18dc075b942c98a5c36e6
Haseeb Ahmad <hasseebmayo@gmail.com>
2024-01-04 16:35:36 +0500

    figma design

---
+++ script.js
@@ -0,0 +1,24 @@
+// Load header and footer components
+window.onload = function () {
+  const dynamicElements = document.querySelectorAll("[data-section]");
+
+  dynamicElements.forEach((section) => {
+    const sectionName = section.getAttribute("data-section");
+    console.log(sectionName);
+
+    fetch(`${sectionName}`)
+      .then((response) => response.text())
+      .then((data) => {
+        section.innerHTML = data;
+      });
+  });
+};
+
+function openNav() {
+  console.log("s");
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
