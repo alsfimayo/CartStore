3d8f5c80aead12d8c602842fb3c1a4f918019974
Haseeb Ahmad <hasseebmayo@gmail.com>
2024-06-11 03:10:34 +0500

    mobile store

--- productpage2/script.js
+++ productpage2/script.js
@@ -1,48 +1,7 @@
-// window.onload = function () {
-//   const dynamicElements = document.querySelectorAll("[data-section]");
-
-//   dynamicElements.forEach((section) => {
-//     const sectionName = section.getAttribute("data-section");
-//     console.log(sectionName);
-
-//     fetch(`${sectionName}`)
-//       .then((response) => response.text())
-//       .then((data) => {
-//         section.innerHTML = data;
-//       });
-//   });
-// };
-
-// function openNav() {
-//   document.getElementById("mySidenav").style.width = "60%";
-// }
-
-// function closeNav() {
-//   document.getElementById("mySidenav").style.width = "0";
-// }
-// function addtoCart() {
-//   var cartValue = localStorage.getItem("products");
-//   if (!localStorage.getItem("products")) {
-//     localStorage.setItem("products", 1);
-//     window.location.reload();
-//     return;
-//   }
-//   console.log(cartValue);
-//   const increment = Number(cartValue) + 1;
-//   localStorage.setItem("products", increment);
-//   console.log("clicked");
-//   window.location.reload();
-// }
-// function showTotalCartOfProduct() {
-//   var cartValue = localStorage.getItem("products");
-//   const cart = document.getElementById("cartproduct");
-//   cart.innerText = cartValue;
-// }
-
-// showTotalCartOfProduct();
-
+//adding cart value
 function addToCart() {
   var cartValue = localStorage.getItem("products");
+  //console.log(cartValue);
 
   if (!cartValue) {
     cartValue = 1;
@@ -51,14 +10,57 @@ function addToCart() {
   }
 
   localStorage.setItem("products", cartValue);
+  // window.location.reload();
   showTotalCartOfProduct();
 }
 
 function showTotalCartOfProduct() {
   var cartValue = localStorage.getItem("products");
-  const cart = document.getElementById("cartproduct");
-  cart.innerText = cartValue;
+  // const cart = document.getElementById("cart");
+  console.log(cartValue);
+  const cart = document.querySelector(".alsfi");
+  if (cart) {
+    cart.textContent = cartValue;
+  }
 }
 
-// Show the initial cart value on page load
-showTotalCartOfProduct();
+window.onload = function () {
+  const dynamicElements = document.querySelectorAll("[data-section]");
+
+  dynamicElements.forEach((section) => {
+    const sectionName = section.getAttribute("data-section");
+    console.log(sectionName);
+
+    fetch(`${sectionName}`)
+      .then((response) => response.text())
+      .then((data) => {
+        section.innerHTML = data;
+      });
+  });
+};
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "60%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+//link header and footer
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("headerContainer").innerHTML = data;
+      showTotalCartOfProduct();
+    });
+  fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footerContainer").innerHTML = data;
+    });
+});
+
+//showTotalCartOfProduct();
